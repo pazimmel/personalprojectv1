d6c5b7321618ed6b14b5acbe387332702e2df410
Paul Zimmel <pazimmel@gmail.com>
2015-11-24 09:13:27 -0600

    paper trail test

--- server/module/quickstart.js
+++ server/module/quickstart.js
@@ -130,33 +130,33 @@ function updateEvents(auth) {
         }
     });
 }
-//function listEvents(auth) {
-//    var calendar = google.calendar('v3');
-//    calendar.events.list({
-//        auth: auth,
-//        calendarId: 'vmte39c86sbcmh9fqr58iglsuo@group.calendar.google.com',
-//        timeMin: (new Date()).toISOString(),
-//        maxResults: 10,
-//        singleEvents: true,
-//        orderBy: 'startTime'
-//    }, function(err, response) {
-//        if (err) {
-//            console.log('The API returned an error: ' + err);
-//            return;
-//        }
-//        var events = response.items;
-//        if (events.length == 0) {
-//            console.log('No upcoming events found.');
-//        } else {
-//            console.log('Upcoming 10 events:');
-//            for (var i = 0; i < events.length; i++) {
-//                var event = events[i];
-//                var start = event.start.dateTime || event.start.date;
-//                var id = event.id;
-//                var personName = event.attendees;
-//                //var email = event.attendees.email;
-//                console.log(id, '%s - %s', start, event.summary, personName);
-//            }
-//        }
-//    });
-//}
+function listEvents(auth) {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: 'vmte39c86sbcmh9fqr58iglsuo@group.calendar.google.com',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming 10 events:');
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                var id = event.id;
+                var personName = event.attendees;
+                //var email = event.attendees.email;
+                console.log(id, '%s - %s', start, event.summary, personName);
+            }
+        }
+    });
+}
