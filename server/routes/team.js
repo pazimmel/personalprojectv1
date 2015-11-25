9d6af55af26477952f13098b18bb9a260e3a7781
Paul Zimmel <pazimmel@gmail.com>
2015-11-25 16:11:48 -0600

    player insert mongo db

--- server/routes/team.js
+++ server/routes/team.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+mongoose.model('Player', new Schema({"name": String, "email": String, "type": String}, {collection: 'roster'}, {autoIndex: false}));
+var Player = mongoose.model('Player');
+
+
 router.post('/', function(req, res){
     var player = req.body;
     console.log(player);
+    player = new Player({name: player.name, email: player.email, type: player.type});
+    player.save(function(err, data){
+        if (err) console.log("Error ", err);
+        res.send(data);
+    });
 
-    res.send("we got the player!");
 });
 
 
