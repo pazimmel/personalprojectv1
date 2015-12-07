0f503754781298406e63ce8aaf44f3d616a8bf1c
Paul Zimmel <pazimmel@gmail.com>
2015-12-06 19:51:27 -0600

    herokuDB try

--- server/module/mongoose.js
+++ server/module/mongoose.js
@@ -2,15 +2,16 @@ var mongoose = require('mongoose');
 
 
 // Mongo Connection //
-//var mongoURI = "mongodb://localhost:27017/team_manager";
+
+var mongoURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017/team_manager" ;
 
 
 
 
 //var mongoURI = "";
 
-//var mongoDB = mongoose.connect(mongoURI).connection;
-var mongoDB = mongoose.connect(MONGOLAB_URI).connection;
+var mongoDB = mongoose.connect(mongoURI).connection;
+
 
 mongoDB.on('error', function(err){
     if(err) console.log("MONGO ERROR: ", err);
